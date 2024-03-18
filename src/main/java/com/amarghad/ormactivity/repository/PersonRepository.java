44311641d017a6ac5b993f18d45a78165cd239d5
x_kroyo <mr.ayoubmarghad@gmail.com>
2024-03-18 14:23:50 +0000

    Add fetch function with pagination

--- src/main/java/com/amarghad/ormactivity/repository/PersonRepository.java
+++ src/main/java/com/amarghad/ormactivity/repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.amarghad.ormactivity.repository;
 
 import com.amarghad.ormactivity.entities.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +15,7 @@ public interface PersonRepository<T extends Person> extends JpaRepository<T, Lon
     List<T> findByFirstNameContains(String firstName);
 
     List<T> findByFirstNameOrLastNameContains(String firstName, String lastName);
+
+    Page<T> findByFirstNameOrLastNameContains(String firstName, String lastName, Pageable pageable);
 
 }
